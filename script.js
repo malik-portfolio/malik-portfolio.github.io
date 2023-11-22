71b20e3cba946e10d0ff6ffe62b4e8e102229847
malik-jahan <malic.jahan@gmail.com>
2023-11-22 15:46:52 +0500

    Bug fix
    Fix menu height on mobile devices

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const menu = document.getElementById("header");
 // Event Listeners
 showMenu.addEventListener("click", () => {
   menu.style.display = "block";
+  document.body.style.overflow = "hidden";
 });
 
 hideMenu.addEventListener("click", () => {
   menu.style.display = "none";
+  document.body.style.overflow = "visible";
 });
