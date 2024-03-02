82c0453a5ddd921f58b6cf9e4bc62522111363e9
malik-jahan <malic.jahan@gmail.com>
2024-03-02 16:00:16 +0500

    Update
    1. Profile picture brightness adjustment
    2. CSS refactor

--- script.js
+++ script.js
@@ -4,16 +4,8 @@ const navLinksEl = document.querySelector('.navigation');
 const navLinksCloseBtn = document.querySelector('.navigation-close-btn');
 
 // Event Listeners
-if (window.innerWidth <= 1000) {
-    menuIconEl.addEventListener('click', () => {
-        navLinksEl.classList.toggle('show');
-    })
 
-    navLinksCloseBtn.addEventListener('click', () => {
-        navLinksEl.classList.remove('show');
-    })
-} else {
-    const pathName = window.location.pathname.slice(1);
-    document.querySelector(`.nav-link a[href='${pathName}']`).style.color = '#eeeeee';
-}
+let pathName = window.location.pathname.slice(1);
+if (pathName === '') pathName = 'index.html';
+document.querySelector(`.nav-link a[href='${pathName}']`).style.color = 'springgreen';
 
