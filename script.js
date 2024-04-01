37c4ba69b6a9147d1dbbbf6d198bee51f2dd6ba5
malik-jahan <malic.jahan@gmail.com>
2024-04-01 18:07:54 +0500

    Update
    Update design inspired by google material design. Add new skills

---
+++ script.js
@@ -0,0 +1,233 @@
+// Data - About
+const aboutData = [
+    {
+        iconName: 'certificate',
+        title: 'Professional certified front end engineer',
+        description: 'A professional certified front-end engineer is a web developer with expertise in the languages and tools that bring websites and applications to life for users.  These certified professionals can craft interactive elements, design user-friendly interfaces, and ensure websites function flawlessly across different devices.'
+    },
+    {
+        iconName: 'server',
+        title: 'Self taught back end engineer ',
+        description: 'A self-taught back-end engineer is a programmer who has acquired the skills to build and maintain the server-side of web applications through independent learning. This involves mastering programming languages, databases, and frameworks to handle data processing, business logic, and communication with the front-end that users interact with.'
+    },
+    {
+        iconName: 'technology',
+        title: 'Modern technologies like react, tailwind and typescript',
+        description: 'Modern web development is shaped by powerful tools like React, Tailwind CSS, and TypeScript. React allows you to build dynamic and reusable user interfaces, while Tailwind CSS provides a utility-based approach for rapidly styling your webpages. TypeScript adds another layer by introducing static typing to JavaScript, catching errors early on and making your code more robust and maintainable. Together, these technologies streamline development, promote cleaner code, and empower you to create modern, interactive web experiences.'
+    },
+    {
+        iconName: 'data_structure',
+        title: 'Understanding of data structures and algorithms',
+        description: 'Understanding data structures and algorithms (DSA) is like having a toolbox for building efficient software. Data structures are specialized ways of organizing data in memory, like using shelves for books or a rolodex for contacts. Algorithms are the step-by-step instructions, like a recipe, that tell the computer how to solve problems using those data structures. By understanding different data structures and algorithms, you can choose the right tools to solve problems quickly and efficiently.'
+    },
+    {
+        iconName: 'user_experience',
+        title: 'Understanding of UI / UX design',
+        description: 'UI/UX design is the combined effort of crafting both the user interface (UI) and the user experience (UX) for a product. UI design focuses on the visual look and feel, including buttons, menus, and layouts, while UX design considers the entire user journey, ensuring the product is intuitive, efficient, and enjoyable to use.  Essentially, UI is what you see, and UX is how it makes you feel.'
+    },
+    {
+        iconName: 'security',
+        title: 'Web security analysis by using burpsuite tool',
+        description: 'Web security analysis with Burp Suite involves using a suite of integrated tools to thoroughly examine a web application for vulnerabilities. Burp Suite acts as an intercepting proxy, capturing all traffic between your browser and the web server. This allows you to analyze requests and responses, manually manipulate data to test for weaknesses, and leverage automated scanning features to identify potential security flaws. By combining these functionalities, Burp Suite empowers you to comprehensively assess an application\'s security posture.'
+    },
+    {
+        iconName: 'team',
+        title: 'Team collaboration by using github',
+        description: 'GitHub fosters team collaboration by centralizing your project\'s codebase.  This allows teammates to work on different parts of the code simultaneously, track changes,  merge edits seamlessly, and  communicate through pull requests to ensure everyone stays on the same page.'
+    },
+    {
+        iconName: 'learn',
+        title: 'Learn new technologies to stay update',
+        description: 'Staying relevant in today\'s ever- evolving tech world requires continuous learning.Embrace your curiosity, set achievable goals, and leverage the wealth of free and paid online resources like courses, tutorials, and communities.Don\'t forget to put your knowledge to practice and share it with others to solidify your understanding and make the learning journey an engaging experience.'
+    },
+];
+
+// Data - Skill
+const skillData = [
+    {
+        iconName: 'figma',
+        title: 'Figma',
+        description: 'Figma is a web-based design tool that lets you create anything from website mockups to app interfaces, all while collaborating with teammates in real-time. It offers a variety of design tools, allows for prototyping to test user interactions, and works seamlessly on any web browser, making it a great choice for both individual designers and design teams.'
+    },
+    {
+        iconName: 'html',
+        title: 'HTML',
+        description: 'HTML, short for HyperText Markup Language, is the building block of web pages. It defines the structure and content of a webpage using tags, which act like instructions for web browsers.  These tags tell the browser how to display text, images, links, and other elements, forming the foundation of the webpages you see every day.'
+    },
+    {
+        iconName: 'css',
+        title: 'CSS',
+        description: 'CSS, short for Cascading Style Sheets, is the styling language for web pages. It controls how HTML elements are displayed, like fonts, colors, layout, and spacing.  By separating style from content (HTML), CSS makes websites easier to maintain and allows for consistent design across multiple pages.'
+    },
+    {
+        iconName: 'javascript',
+        title: 'JavaScript',
+        description: 'JavaScript is the programming language that brings web pages to life. It\'s a versatile tool used to create dynamic features you see everywhere online, from interactive menus to animations.Unlike static HTML content, JavaScript code can react to user input and update the page in real- time, making web browsing a more engaging experience.'
+    },
+    {
+        iconName: 'sql',
+        title: 'SQL',
+        description: 'SQL, which stands for Structured Query Language, is a standardized way to communicate with relational databases.  Think of it as a special language you use to ask the database to store, retrieve, update, or delete information. It\'s used by many popular database management systems and allows you to efficiently manage and analyze all sorts of data.'
+    },
+    {
+        iconName: 'react',
+        title: 'React',
+        description: 'React is a free and popular JavaScript library for building user interfaces. It breaks down complex interfaces into reusable building blocks called components, making development faster and easier to maintain. React excels at efficiently updating only the parts of the screen that change, resulting in smooth and responsive user experiences.'
+    },
+    {
+        iconName: 'redux',
+        title: 'Redux',
+        description: 'Redux is a predictable state management tool for Javascript applications. It acts like a central storage unit for your app\'s data, ensuring a single source of truth.By using actions and reducers, Redux keeps track of changes to the data in a predictable way, making it easier to reason about how your application behaves and simplifies collaboration on large projects.'
+    },
+    {
+        iconName: 'tailwind',
+        title: 'Tailwind CSS',
+        description: 'Tailwind CSS is a unique CSS framework that breaks from the mold by offering a utility-first approach. Instead of pre-built components, it provides a massive toolbox of low-level utility classes for things like spacing, flexbox, and typography. These classes can be directly applied in your HTML, allowing for rapid development and complete design customization.'
+    },
+    {
+        iconName: 'nextjs',
+        title: 'Next JS',
+        description: 'Next.js is a popular framework built on React that simplifies building fast and SEO-friendly web applications. It extends React by offering features like server-side rendering for improved performance, static site generation for SEO benefits, and built-in functionalities for routing, data fetching, and image optimization, making development with React more efficient.'
+    },
+    {
+        iconName: 'nodejs',
+        title: 'Node JS',
+        description: 'Node.js is a free, open-source platform that lets developers use JavaScript to build server-side applications and tools.  Unlike traditional JavaScript that runs in web browsers, Node.js allows you to write JavaScript code that executes on a computer server, making it a versatile tool for creating dynamic web applications and real-time services.'
+    },
+    {
+        iconName: 'expressjs',
+        title: 'Express JS',
+        description: 'Express.js is a popular open-source framework for building web applications and APIs with Node.js.  Known for its flexibility and minimalism, it provides a robust set of features for handling web requests, responses, and middleware. This streamlined approach makes it easy to create efficient and scalable web applications,  particularly RESTful APIs, all while leveraging the power of JavaScript.'
+    },
+    {
+        iconName: 'mongodb',
+        title: 'MongoDB',
+        description: 'MongoDB is a flexible, document-based database that stores data in JSON-like documents. Unlike traditional relational databases, it offers scalability and ease of use for modern applications.  MongoDB excels at handling large amounts of data and is particularly suited for projects where data structure might evolve over time.'
+    },
+    {
+        iconName: 'typescript',
+        title: 'TypeScript',
+        description: 'TypeScript is an extension of JavaScript that adds optional typing features. This lets you define the data types of variables and functions, improving code clarity and catching errors early in the development process.  Though it compiles down to regular JavaScript, TypeScript offers better tooling and makes large JavaScript projects more manageable.'
+    },
+    {
+        iconName: 'python',
+        title: 'Python',
+        description: 'Python is a versatile, beginner-friendly programming language known for its clear and readable code.  This high-level language is used for everything from building websites and software to data science and automation.  Its flexibility and wide range of applications make it a popular choice for programmers of all experience levels.'
+    },
+    {
+        iconName: 'git',
+        title: 'Git',
+        description: 'Git is a version control system that tracks changes in your code or files over time. It lets you revert to previous versions if needed, collaborate by sharing changes with others, and see the history of your project. Imagine it like a time machine for your code, allowing you to travel back and see how it evolved.'
+    },
+    {
+        iconName: 'vscode',
+        title: 'VS Code',
+        description: 'Visual Studio Code, or VS Code, is a free, open-source code editor from Microsoft.  It\'s popular for its lightweight design, yet powerful features like syntax highlighting, intelligent code completion, and built-in Git version control. VS Code works across Windows, macOS, and Linux, and its vast extension marketplace lets you customize it for nearly any programming language or development workflow.'
+    },
+    {
+        iconName: 'burpsuite',
+        title: 'Burp Suite',
+        description: 'Burp Suite is a popular platform for web application security testing. It acts as an all-in-one toolbox, offering features like intercepting and modifying traffic (proxy), automatically discovering website elements (spider), testing for vulnerabilities (scanner), and analyzing repeated requests (repeater). This comprehensive suite helps identify weaknesses in web applications, making it a valuable tool for security professionals and bug bounty hunters.'
+    },
+    {
+        iconName: 'postman',
+        title: 'Postman',
+        description: 'Postman is a software platform specifically designed to simplify working with APIs (Application Programming Interfaces). It acts as a hub for building, testing, and managing APIs throughout their lifecycle.  Postman helps developers send requests, analyze responses, collaborate on designs, and streamline the entire API development process.'
+    },
+];
+
+// Data - Project
+const projectData = [
+    {
+        iconName: 'game_icon',
+        title: 'Number Magic Game',
+        description: 'In this game, the user thinks of a number in a range. The computer will show some number grid slides and ask that the number is available in this slide. After getting answers from the user,                    the computer will guess the number and display it.',
+        link: 'https://malik-portfolio.github.io/number-magic-game'
+    }
+];
+
+// Map section to data
+const sectionDataMap = {
+    'about': aboutData,
+    'skill': skillData,
+    'project': projectData
+}
+
+// Global Variables
+let currentSection = 'about';
+
+// Element Selection
+const cardsWrapper = document.getElementById('cards-wrapper');
+const navSection = document.getElementById('nav-section');
+const cardModalWrapper = document.getElementById('card-modal-wrapper');
+const cardModal = document.getElementById('card-modal');
+
+// Event Listener
+navSection.addEventListener('click', (event) => {
+    document.getElementById(currentSection).classList.remove('gradient-background');
+    currentSection = event.target.title;
+    document.getElementById(currentSection).classList.add('gradient-background');
+    renderCardCollection();
+})
+
+cardModalWrapper.addEventListener('click', () => {
+    cardModalWrapper.style.display = 'none';
+})
+
+cardsWrapper.addEventListener('click', renderModal);
+
+// Utilites
+function renderCardCollection() {
+    const data = sectionDataMap[currentSection];
+    const cardCollection = data.map((item) => {
+        const { iconName, title, description, link } = item;
+        const cardHtml = generateCardHtml(iconName, title, description, link);
+        return cardHtml
+    }).join('');
+    cardsWrapper.innerHTML = cardCollection;
+}
+
+function renderModal(event) {
+    // Find Card Element
+    const targetEl = event.target
+    const cardEl = targetEl.closest('#card')
+
+    // Check if card element is not rendered
+    if (!cardEl) {
+        return;
+    }
+
+    // If rendered than show card modal
+    const description = cardEl.dataset.description;
+    const link = cardEl.dataset.link;
+    const iconName = cardEl.dataset.iconname
+    console.log(cardEl)
+    const cardModalHtml = `
+        <div class="card-icon-mobile bg-${iconName}"></div>  
+        <p id="card-modal-description">${description}</p>
+        ${link ? `
+            <div id="project-technologies">React - Tailwind CSS - TypeScript</div>  
+            <a href="${link}" id="project-link" target="_blank">Visit</a>
+        ` : ''}
+    `;
+    cardModalWrapper.style.display = 'flex';
+    cardModal.innerHTML = cardModalHtml;
+}
+
+function generateCardHtml(iconName, title, description, link) {
+    return `
+        <div 
+            class="card"
+            id="card" 
+            data-description="${description}" 
+            data-iconName="${iconName}" 
+            ${link && 'data-link="' + link + '"'}>
+                <div>
+                    <span class="card-icon bg-${iconName}"></span>
+                </div>
+                <h2 class="card-title">${title}</h2>
+        </div>`;
+}
+
+// Main
+renderCardCollection();
